bafb6b8eca7ab988889409cc145e33081e3d9bd8
Alex <alex.wallerand@gmail.com>
2023-01-25 09:03:00 +0100

    Add encryption of data

--- encrypted_linear_regression.py
+++ encrypted_linear_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from homomorphic import *
+from Pyfhel import Pyfhel
 
 def init_theta(data):
     return np.zeros((data.shape[1], 1))
@@ -24,6 +25,7 @@ def gradient_descent(X, y, theta, learning_rate, n_iteration):
 
 def regression(X, y):
     theta = init_theta(X)
+    cyphertext_theta = matrice_encrypt(theta)
     theta_f = gradient_descent(X, y, theta, learning_rate=0.1, n_iteration=1000)
     return model(X, theta_f)
 
@@ -37,9 +39,24 @@ def load_data(path, column1, column2):
 
 #~~~~~~~~~~~~~~~~~~~~ Programme Principal ~~~~~~~~~~~~~~~~~~~~
 
-x, X, y = load_data('Car_sales.csv','Engine_size','Horsepower')
-regression = regression(X, y)
+# Initialisation du modèle d'encryption Pyfhel
+HE = Pyfhel()
+HE.contextGen(scheme="ckks", n=2**14, scale=2**30, qi_sizes=[60, 30, 30, 30, 60])
+HE.keyGen()
+HE.rotatekeyGen()
 
-plt.scatter(x, y)
+# Récupération des données
+x, X, y = load_data('Car_sales.csv','Engine_size','Horsepower')
+plaintext_x = np.array(X, dtype=np.float64)
+plaintext_y = np.array(y, dtype=np.float64)
+
+# Encryption des données
+cyphertext_x = matrice_encrypt(plaintext_x)
+cyphertext_y = matrice_encrypt(plaintext_y)
+
+# Calcul de la régression cryptée
+regression = regression(cyphertext_x, cyphertext_y).tolist()
+
+"""plt.scatter(x, y)
 plt.plot(x, regression, color='green')
-plt.show()
+plt.savefig("result.png")"""
